b5fb8d378d04b08a0a9324d456497e8f62b2dda8
anabotr <ana.botr@gmail.com>
2024-11-22 12:17:55 -0300

    Added new articles
    The content is all complete, there are some design adjustments needed to complete the content

--- site_matemática/script.js
+++ site_matemática/script.js
@@ -1,18 +1,15 @@
+//função para passar os gifs pelo botão
 const gif = document.getElementById("gifs");
-let indice = 0
-let gifslista = ["gifs/200w.gif", "gifs/animacao-pi.gif", "gifs/espiral-áurea-hcc.gif", "gifs/gif-matematico-12.gif", "gifs/gif-matematico-13.gif", "gifs/gif-matematico-25.gif", "gifs/gif-matematico-31.gif"]
-
-
+var indice = 0
+let gifslista = ["gifs/espiral-áurea-hcc.gif", "gifs/animacao-pi.gif", "gifs/200w.gif", "gifs/gif-matematico-12.gif", "gifs/gif-matematico-13.gif", "gifs/gif-matematico-25.gif", "gifs/gif-matematico-31.gif"]
 
 const botaoesquerdagif = document.getElementById('botaoesquerdagif');
 botaoesquerdagif.addEventListener('click', passagifesquerda);
 
-
-
 function passagifesquerda(){
   if(indice > 0){
     indice -= 1;
-    gif.src = gifslista[indice];
+    gif.src = gifslista[indice]
   }    
 }
 
@@ -22,8 +19,23 @@ botaodireitagif.addEventListener('click', passagifdireita);
 function passagifdireita(){
   if(indice < 6){
     indice += 1;
-    gif.src = gifslista[indice];
-    console.log(indice);
+    gif.src = gifslista[indice]
   }
 
 }
+
+
+
+//função para passar os gifs pelo tempo
+var avancar = true
+  setInterval(function () {
+    if(indice < 6){
+      indice += 1
+      gif.src = gifslista[indice]
+    }
+    else{
+      indice = 0
+      gif.src = gifslista[indice]
+    }
+
+  }, 15000);
